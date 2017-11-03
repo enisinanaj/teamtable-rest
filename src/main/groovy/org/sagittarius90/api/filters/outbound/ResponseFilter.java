5e3b7563ffe6847d6b8916961207f5bfbd025e18
ivanvidale <ivan.vidale@gmail.com>
2017-11-04 00:41:48 +0100

    Added ResponseFilter java class

---
+++ src/main/groovy/org/sagittarius90/api/filters/outbound/ResponseFilter.java
@@ -0,0 +1,15 @@
+package org.sagittarius90.api.filters.outbound;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class ResponseFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("X-Powered-By", "HeiseNerds");
+    }
+}
