a78fce14e1eb0a7c3c440220ee4951edc7ca99ec
Eni Sinanaj <enisinanaj@outlook.com>
2017-11-12 18:27:26 +0100

    added CORS implementation

---
+++ src/main/groovy/org/sagittarius90/api/filters/outbound/CORSResponseFilter.java
@@ -0,0 +1,25 @@
+package org.sagittarius90.api.filters.outbound;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CORSResponseFilter
+        implements ContainerResponseFilter {
+
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+            throws IOException {
+
+        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+
+        headers.add("Access-Control-Allow-Origin", "*");
+        //headers.add("Access-Control-Allow-Origin", "http://localhost");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Codingpedia");
+    }
+
+}
